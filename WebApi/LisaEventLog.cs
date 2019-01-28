f1cd682bb511ec4d7aeb276177704ea1f05d1b73
Chuck Lu <chuck.lu@qq.com>
2019-01-28 10:10:25 +0800

    add event log class

---
+++ WebApi/LisaEventLog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WebApi
+{
+    public class LisaEventLog
+    {
+        private readonly string _logName = @"Chuck";
+
+        public string LogName => _logName;
+
+        public LisaEventLog()
+        {
+        }
+
+        public LisaEventLog(string logName)
+        {
+            _logName = logName;
+        }
+
+        public void WriteEntry(string error, EventLogEntryType type)
+        {
+            var sourceName = AppDomain.CurrentDomain.FriendlyName;
+            if (!EventLog.SourceExists(sourceName))
+            {
+                EventLog.CreateEventSource(sourceName, _logName);
+            }
+            using (EventLog eventLog = new EventLog(_logName))
+            {
+                eventLog.Source = sourceName;
+                var message = $"{AppDomain.CurrentDomain.BaseDirectory}{Environment.NewLine}{error}";
+                eventLog.WriteEntry(message, type);
+            }
+        }
+    }
+}
