887cbe94483c15ca01a2f1c405230ecfc4913a08
Chuck Lu <chuck.lu@qq.com>
2019-01-31 13:50:16 +0800

    create a new project with individual accounts authentication

---
+++ WebApiWithTemplate/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiWithTemplate.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
